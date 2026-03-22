715f5873897db5b1305cc57dec36fa48257c0fda
Shlok Anand <152406547+ShlokAnand@users.noreply.github.com>
2026-03-22 12:08:34 +0530

    Add files via upload

---
+++ Task3/test_form.py
@@ -0,0 +1,32 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+driver = webdriver.Chrome()
+
+driver.get("file:///C:/Users/Shlok Anand/Desktop/DevOps/task3/index.html")
+
+time.sleep(2)
+
+driver.find_element(By.ID,"name").send_keys("Shlok Anand")
+time.sleep(1)
+
+driver.find_element(By.ID,"email").send_keys("shlokanand@gmail.com")
+time.sleep(1)
+
+driver.find_element(By.ID,"password").send_keys("123456")
+time.sleep(1)
+
+driver.find_element(By.ID,"confirm").send_keys("123456")
+time.sleep(1)
+
+driver.find_elements(By.NAME,"gender")[0].click()
+time.sleep(1)
+
+driver.find_element(By.TAG_NAME,"button").click()
+
+print("✅ TEST PASSED")
+
+time.sleep(3)
+
+driver.quit()
